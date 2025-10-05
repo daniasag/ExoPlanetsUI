b5908ec46d959af735f4deb1bd0d1a717491137b
daniasag <a01567218@tec.mx>
2025-10-05 15:19:20 -0600

    Hash for public domain

--- exo-planets-ui/src/App.js
+++ exo-planets-ui/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import PredictionPage from './components/PredictionPage';
 
